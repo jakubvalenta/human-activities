d32f888f4e63510e41230caffde2e50240eac937
Jakub Valenta <jakub@jakubvalenta.cz>
2018-11-10 14:52:32 +0100

    Working menu (can't use actions in StatusIcon)

--- lidske_aktivity/app.py
+++ lidske_aktivity/app.py
@@ -17,76 +17,52 @@ class AppError(Exception):
     pass
 
 
-class StatusIcon(Gtk.StatusIcon):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
 class Window(Gtk.ApplicationWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        box = Gtk.Box(spacing=6)
-
-        button_1 = Gtk.Button('About')
-        button_1.show()
-        button_1.set_action_name('app.about')
-        box.add(button_1)
-
-        button_2 = Gtk.Button('Quit')
-        button_2.show()
-        button_2.set_action_name('app.quit')
-        box.add(button_2)
-
-        button_2 = Gtk.Button('Menu')
-        button_2.show()
-        button_2.set_action_name('app.menu')
-        box.add(button_2)
-
-        self.add(box)
-        box.show()
+        label = Gtk.Label('Hello world')
+        label.show()
+        self.add(label)
 
 
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, application_id='org.example.myapp', **kwargs)
-        self.status_icon = None
         self.window = None
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
-
-        action = Gio.SimpleAction.new('about', None)
-        action.connect('activate', self.on_about)
-        self.add_action(action)
-
-        action = Gio.SimpleAction.new('quit', None)
-        action.connect('activate', self.on_quit)
-        self.add_action(action)
-
-        action = Gio.SimpleAction.new('menu', None)
-        action.connect('activate', self.on_menu)
-        self.add_action(action)
-
-        builder = Gtk.Builder.new_from_file(XML)
-        menu_model = builder.get_object('popup-menu')
-        self.menu = Gtk.Menu.new_from_model(menu_model)
-
-        self.set_app_menu(menu_model)
+        self.create_menu()
+        self.create_status_icon()
 
     def do_activate(self):
-        self.status_icon = Gtk.StatusIcon.new_from_stock(Gtk.STOCK_HOME)
-        self.status_icon.set_tooltip_text('Lidské aktivity')
-        # if not self.status_icon.is_embedded():
-        #    raise AppError('Tray icon is not supported on this platform')
-        self.status_icon.connect('popup-menu', self.on_status_icon_popup_menu)
-
         # TODO: Remove this window
         if not self.window:
             self.window = Window(application=self, title='Main Window')
         self.window.present()
 
-    def on_status_icon_popup_menu(self, icon, button, time):
+    def create_menu(self):
+        menu = Gtk.Menu()
+        menu_item = Gtk.MenuItem()
+        menu_item.set_label('About')
+        menu_item.connect('activate', self.on_about)
+        menu.append(menu_item)
+        menu_item = Gtk.MenuItem()
+        menu_item.set_label('Quit')
+        menu_item.connect('activate', self.on_quit)
+        menu.append(menu_item)
+        menu.show_all()
+        self.menu = menu
+
+    def create_status_icon(self):
+        status_icon = Gtk.StatusIcon.new_from_stock(Gtk.STOCK_HOME)
+        status_icon.set_tooltip_text('Lidské aktivity')
+        # if not self.status_icon.is_embedded():
+        #    raise AppError('Tray icon is not supported on this platform')
+        status_icon.connect('popup-menu', self.on_menu)
+        self.status_icon = status_icon
+
+    def on_menu(self, icon, button, time):
         self.menu.popup(
             parent_menu_shell=None,
             parent_menu_item=None,
@@ -95,14 +71,11 @@ class Application(Gtk.Application):
             button=button,
             activate_time=time)
 
-    def on_menu(self, action, param):
-        self.menu.popup_at_pointer(None)
-
-    def on_about(self, action, param):
+    def on_about(self, param):
         about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
         about_dialog.present()
 
-    def on_quit(self, action, param):
+    def on_quit(self, param):
         self.quit()
 
 
