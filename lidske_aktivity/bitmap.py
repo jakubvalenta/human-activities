950406bc5ee1637cb2f190efad86b29fd5ab73a6
Jakub Valenta <jakub@jakubvalenta.cz>
2018-12-09 13:58:56 +0100

    bitmap: Only change color hue when generating it

--- lidske_aktivity/bitmap.py
+++ lidske_aktivity/bitmap.py
@@ -24,14 +24,36 @@ def hash_to_fraction(s: str) -> float:
     return fraction
 
 
-def rgb_int_to_tuple(rgb_int: int) -> TColor:
-    return rgb_int & 255, (rgb_int >> 8) & 255, (rgb_int >> 16) & 255
+def hue_to_rgb(p: float, q: float, t: float) -> float:
+    if t < 0:
+        t += 1
+    if t > 1:
+        t -= 1
+    if t < 1/6:
+        return p + (q - p) * 6 * t
+    if t < 1/2:
+        return q
+    if t < 2/3:
+        return p + (q - p) * (2/3 - t) * 6
+    return p
+
+
+def hsl_to_rgb(h: float, s: float, l: float) -> TColor:
+    """https://stackoverflow.com/a/9493060"""
+    if s == 0:
+        r = g = b = l  # achromatic
+    else:
+        q = l * (1 + s) if l < 0.5 else l + s - l * s
+        p = 2 * l - q
+        r = hue_to_rgb(p, q, h + 1/3)
+        g = hue_to_rgb(p, q, h)
+        b = hue_to_rgb(p, q, h - 1/3)
+    return round(r * 255), round(g * 255), round(b * 255)
 
 
 def str_to_color(s: str) -> TColor:
     fraction = hash_to_fraction(s)
-    rgb_int = round(0xffffff * fraction)
-    return rgb_int_to_tuple(rgb_int)
+    return hsl_to_rgb(fraction, 0.8, 0.5)
 
 
 def pie_chart_shader(x: int,
