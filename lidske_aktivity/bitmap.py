62a0c42a9928c86bc22f9811fa9da47af40ffe6e
Jakub Valenta <jakub@jakubvalenta.cz>
2018-12-09 18:55:16 +0100

    bitmap: Cleanup and remove svg

--- lidske_aktivity/bitmap.py
+++ lidske_aktivity/bitmap.py
@@ -5,16 +5,16 @@ from typing import Callable, Iterator, List, Tuple
 
 from PIL import Image
 
-TColor = str
+TColor = Tuple[int, int, int]
 TSliceFrac = Tuple[int, TColor]
 TSliceRad = Tuple[float, float, TColor]
 
 
-def frac_to_rad(frac: int) -> float:
+def _frac_to_rad(frac: int) -> float:
     return frac * 2 * math.pi
 
 
-def hash_to_fraction(s: str) -> float:
+def _hash_to_fraction(s: str) -> float:
     """Hash a string into a floating point number between 0 and 1."""
     hash_obj = sha1(s.encode())
     hash_hex_len = hash_obj.digest_size * 2
@@ -24,7 +24,7 @@ def hash_to_fraction(s: str) -> float:
     return fraction
 
 
-def hue_to_rgb(p: float, q: float, t: float) -> float:
+def _hue_to_rgb(p: float, q: float, t: float) -> float:
     if t < 0:
         t += 1
     if t > 1:
@@ -38,30 +38,30 @@ def hue_to_rgb(p: float, q: float, t: float) -> float:
     return p
 
 
-def hsl_to_rgb(h: float, s: float, l: float) -> TColor:
+def _hsl_to_rgb(h: float, s: float, l: float) -> TColor:
     """https://stackoverflow.com/a/9493060"""
     if s == 0:
         r = g = b = l  # achromatic
     else:
         q = l * (1 + s) if l < 0.5 else l + s - l * s
         p = 2 * l - q
-        r = hue_to_rgb(p, q, h + 1/3)
-        g = hue_to_rgb(p, q, h)
-        b = hue_to_rgb(p, q, h - 1/3)
+        r = _hue_to_rgb(p, q, h + 1/3)
+        g = _hue_to_rgb(p, q, h)
+        b = _hue_to_rgb(p, q, h - 1/3)
     return round(r * 255), round(g * 255), round(b * 255)
 
 
 def str_to_color(s: str) -> TColor:
-    fraction = hash_to_fraction(s)
-    return hsl_to_rgb(fraction, 0.8, 0.5)
+    fraction = _hash_to_fraction(s)
+    return _hsl_to_rgb(fraction, 0.8, 0.5)
 
 
-def pie_chart_shader(x: int,
-                     y: int,
-                     w: int,
-                     h: int,
-                     slices_rad: List[TSliceRad],
-                     default_color: str = (255, 255, 255)) -> TColor:
+def _pie_chart_shader(x: int,
+                      y: int,
+                      w: int,
+                      h: int,
+                      slices_rad: List[TSliceRad],
+                      default_color: TColor = (255, 255, 255)) -> TColor:
     center = w / 2, h / 2
     radius = min(center)
     coord = x - center[0], y - center[1]
@@ -77,10 +77,10 @@ def pie_chart_shader(x: int,
     return default_color
 
 
-def draw_image(w: int,
-               h: int,
-               shader: Callable,
-               background_color: TColor = '#000000') -> Image:
+def _draw_image(w: int,
+                h: int,
+                shader: Callable,
+                background_color: TColor = (0, 0, 0)) -> Image:
     image = Image.new('RGB', (w, h), background_color)
     pixels = image.load()
     for x in range(w):
@@ -93,8 +93,8 @@ def _slices_frac_to_rad(slices_frac: List[TSliceFrac]) -> Iterator[TSliceRad]:
     cumulative_frac = 0
     for frac, color in slices_frac:
         yield (
-            frac_to_rad(cumulative_frac),
-            frac_to_rad(cumulative_frac + frac),
+            _frac_to_rad(cumulative_frac),
+            _frac_to_rad(cumulative_frac + frac),
             color
         )
         cumulative_frac += frac
@@ -102,21 +102,8 @@ def _slices_frac_to_rad(slices_frac: List[TSliceFrac]) -> Iterator[TSliceRad]:
 
 def draw_pie_chart(size: int, slices_frac: List[TSliceFrac]) -> Image:
     slices_rad = list(_slices_frac_to_rad(slices_frac))
-    return draw_image(
+    return _draw_image(
         w=size,
         h=size,
-        shader=partial(pie_chart_shader, slices_rad=slices_rad)
+        shader=partial(_pie_chart_shader, slices_rad=slices_rad)
     )
-
-
-if __name__ == '__main__':
-    image = draw_pie_chart(
-        size=100,
-        slices_frac=[
-            (0.5, (255, 0, 0)),
-            (0.3, (0, 255, 0)),
-            (0.1, (0, 0, 255)),
-            (0.05, (255, 255, 0)),
-        ]
-    )
-    image.save('tmp.png')
