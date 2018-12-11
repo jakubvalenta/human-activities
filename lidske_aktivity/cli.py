16a64c7775d69c87ffc8c02f74d6ab3080d440f8
Jakub Valenta <jakub@jakubvalenta.cz>
2018-12-11 11:42:58 +0100

    cli: Remove dependency on click, use argparse

--- lidske_aktivity/cli.py
+++ lidske_aktivity/cli.py
@@ -1,23 +1,23 @@
+import argparse
 import logging
 import sys
-
-import click
 
 from lidske_aktivity import app, config
 
 logger = logging.getLogger(__name__)
 
 
-@click.command()
-@click.option('-v', '--verbose', is_flag=True)
-@click.option('-c', '--clean', is_flag=True)
-def main(verbose: bool, clean: bool):
-    if verbose:
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-c', '--clean', action='store_true')
+    args = parser.parse_args()
+    if args.verbose:
         logging.basicConfig(
             stream=sys.stdout,
             level=logging.INFO,
             format='%(message)s')
-    if clean:
+    if args.clean:
         config.clean_cache()
     else:
         app.main()
