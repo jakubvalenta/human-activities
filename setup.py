4ffc95ce87339fb836adec8a212f484ce2483e6c
Jakub Valenta <jakub@jakubvalenta.cz>
2018-12-29 18:42:30 +0100

    setup.py: Add

---
+++ setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+from lidske_aktivity import (
+    __author_email__, __authors__, __license__, __summary__, __uri__,
+    __version__,
+)
+
+setup(
+    name='lidske-aktivity',
+    version=__version__,
+    description=__summary__,
+    long_description=(Path(__file__).parent / 'README.md').read_text(),
+    author=__authors__[0],
+    author_email=__author_email__,
+    url=__uri__,
+    license=__license__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'pillow',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lidske-aktivity=lidske_aktivity.cli:main',
+        ],
+    },
+)
