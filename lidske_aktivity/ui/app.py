111e72c3b4057adcf4229eba854e3ee8a4992613
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-07 22:28:47 +0100

    ui/wx: Create App and StatusIcon

--- lidske_aktivity/ui/app.py
+++ lidske_aktivity/ui/app.py
@@ -3,8 +3,7 @@ from threading import Event, Thread
 from time import sleep
 from typing import Callable, Optional
 
-import wx
-import wx.adv
+from PIL import Image
 
 from lidske_aktivity import (
     __authors__, __copyright__, __title__, __uri__, __version__,
@@ -12,75 +11,53 @@ from lidske_aktivity import (
 from lidske_aktivity.bitmap import draw_pie_chart, gen_random_slices
 from lidske_aktivity.config import save_config
 from lidske_aktivity.store import Store, TFractions
-from lidske_aktivity.ui.lib import create_icon_from_image, new_id_ref_compat
+from lidske_aktivity.ui.lib import App, StatusIcon
 from lidske_aktivity.ui.menu import Menu
 from lidske_aktivity.ui.settings import Settings
-from lidske_aktivity.ui.setup import Setup
 
 logger = logging.getLogger(__name__)
 
 
-class TaskBarIcon(wx.adv.TaskBarIcon):
+class StatusIcon(StatusIcon):
     store: Store
     last_percents: Optional[TFractions] = None
-    id_setup = new_id_ref_compat()
 
-    def __init__(self,
-                 store: Store,
-                 on_menu: Callable,
-                 on_setup: Callable,
-                 on_settings: Callable,
-                 on_about: Callable,
-                 on_quit: Callable):
+    def __init__(self, store: Store, *args, **kwargs):
         self.store = store
-        # TODO: Check TaskBarIcon.isAvailable()
-        super().__init__(wx.adv.TBI_DOCK)
-        self.update()
-        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, on_menu)
-        self.Bind(wx.EVT_MENU, on_setup, id=self.id_setup)
-        self.Bind(wx.EVT_MENU, on_settings, id=wx.ID_SETUP)
-        self.Bind(wx.EVT_MENU, on_about, id=wx.ID_ABOUT)
-        self.Bind(wx.EVT_MENU, on_quit, id=wx.ID_EXIT)
+        super().__init__(*args, **kwargs)
 
-    def CreatePopupMenu(self) -> wx.Menu:
-        menu = wx.Menu()
-        menu.Append(self.id_setup, '&Setup')
-        menu.Append(wx.ID_SETUP, 'Advanced &configuration')
-        menu.Append(wx.ID_ABOUT, '&About')
-        menu.Append(wx.ID_EXIT, 'E&xit')
-        return menu
-
-    def update(self):
+    def _should_update(self) -> bool:
         if self.store.percents == self.last_percents:
-            return
+            return False
         self.last_percents = self.store.percents
+        return True
+
+    def _create_icon_image(self) -> Image:
         slices_frac = list(self.store.percents.values())
         logger.info('Updating icon with slices %s', slices_frac)
-        image = draw_pie_chart(self.calc_icon_size(), slices_frac)
-        icon = create_icon_from_image(image)
-        tooltip = '\n'.join(
+        return draw_pie_chart(self.calc_icon_size(), slices_frac)
+
+    def _create_tooltip(self) -> str:
+        return '\n'.join(
             '{text}: {fraction:.2%}'.format(
                 text=self.store.get_text(path),
                 fraction=fraction
             )
             for path, fraction in self.store.percents.items()
         )
-        self.SetIcon(icon, tooltip)
 
-    @staticmethod
-    def calc_icon_size() -> int:
-        if 'wxMSW' in wx.PlatformInfo:
-            return 16
-        if 'wxGTK' in wx.PlatformInfo:
-            return 22
-        return 128
+    def update(self):
+        if self._should_update():
+            image = self._create_icon_image()
+            tooltip = self._create_tooltip()
+            self.update_icon(image, tooltip)
 
 
-class Application(wx.App):
+class Application(App):
     menu: Menu
     store: Store
     on_quit: Callable
-    status_icon: TaskBarIcon
+    status_icon: StatusIcon
     tick_event_stop: Optional[Event] = None
     tick_thread: Optional[Thread] = None
 
@@ -94,8 +71,7 @@ class Application(wx.App):
         super().__init__(False, *args, **kwargs)
 
     def OnInit(self) -> bool:
-        self.frame = wx.Frame(parent=None, title=__title__)
-        self.status_icon = TaskBarIcon(
+        self.status_icon = StatusIcon(
             store=self.store,
             on_menu=lambda event: self.show_menu(),
             on_setup=lambda event: self.show_setup(),
@@ -103,6 +79,7 @@ class Application(wx.App):
             on_about=lambda event: self.show_about(),
             on_quit=lambda event: self.quit()
         )
+        self.frame = wx.Frame(parent=None, title=__title__)
         self.menu = Menu(store=self.store, parent=self.frame)
         if self.store.config.show_setup:
             self.store.config.show_setup = False
