02174fd3416e9868abef6bea568e0cbabd4dcbd6
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-07 23:44:35 +0100

    ui/app: Finish moving App and StatusIcon to lib

--- lidske_aktivity/ui/app.py
+++ lidske_aktivity/ui/app.py
@@ -1,6 +1,4 @@
 import logging
-from threading import Event, Thread
-from time import sleep
 from typing import Callable, Optional
 
 from PIL import Image
@@ -12,20 +10,70 @@ from lidske_aktivity.bitmap import draw_pie_chart, gen_random_slices
 from lidske_aktivity.config import save_config
 from lidske_aktivity.store import Store, TFractions
 from lidske_aktivity.ui.lib import Application, StatusIcon, show_about
-from lidske_aktivity.ui.menu import Menu
 from lidske_aktivity.ui.settings import Settings
 from lidske_aktivity.ui.setup import Setup
 
 logger = logging.getLogger(__name__)
 
 
-class StatusIcon(StatusIcon):
+class Application(Application):
+    title = __title__
+
     store: Store
+    on_quit: Callable
+    status_icon: StatusIcon
     last_percents: Optional[TFractions] = None
 
-    def __init__(self, store: Store, *args, **kwargs):
+    def __init__(self, store: Store, on_quit: Callable, *args, **kwargs):
         self.store = store
+        self.on_quit = on_quit  # type: ignore
         super().__init__(*args, **kwargs)
+
+    def on_init(self):
+        self.status_icon = StatusIcon(
+            on_setup=self.show_setup,
+            on_settings=self.show_settings,
+            on_about=self.show_about,
+            on_quit=self.quit
+        )
+        if self.store.config.show_setup:
+            self.store.config.show_setup = False
+            self.show_setup()
+
+    def show_setup(self):
+        Setup(
+            self.store.config,
+            on_finish=self.on_setup_finish,
+            parent=self.frame
+        )
+
+    def on_setup_finish(self, setup: Setup):
+        self.store.config = setup.config
+        self.status_icon.refresh_menu()
+        save_config(self.store.config)
+
+    def show_settings(self):
+        Settings(
+            self.store.config,
+            self.on_settings_accept,
+            parent=self.frame
+        )
+
+    def on_settings_accept(self, settings: Settings):
+        self.store.config = settings.config
+        self.status_icon.refresh_menu()
+        save_config(self.store.config)
+
+    def show_about(self):
+        image = draw_pie_chart(148, list(gen_random_slices(3, 8)))
+        show_about(
+            image=image,
+            title=__title__,
+            version=__version__,
+            copyright=__copyright__,
+            uri=__uri__,
+            authors=__authors__
+        )
 
     def _should_update(self) -> bool:
         if self.store.percents == self.last_percents:
@@ -47,120 +95,19 @@ class StatusIcon(StatusIcon):
             for path, fraction in self.store.percents.items()
         )
 
-    def update(self):
+    def on_tick(self, pulse: bool = True):
         if self._should_update():
             image = self._create_icon_image()
             tooltip = self._create_tooltip()
             self.update_icon(image, tooltip)
-
-
-class Application(App):
-    menu: Menu
-    store: Store
-    on_quit: Callable
-    status_icon: StatusIcon
-    tick_event_stop: Optional[Event] = None
-    tick_thread: Optional[Thread] = None
-
-    def __init__(self,
-                 store: Store,
-                 on_quit: Callable,
-                 *args,
-                 **kwargs):
-        self.store = store
-        self.on_quit = on_quit  # type: ignore
-        super().__init__(False, *args, **kwargs)
-
-    def OnInit(self) -> bool:
-        self.status_icon = StatusIcon(
-            store=self.store,
-            on_menu=lambda event: self.show_menu(),
-            on_setup=lambda event: self.show_setup(),
-            on_settings=lambda event: self.show_settings(),
-            on_about=lambda event: self.show_about(),
-            on_quit=lambda event: self.quit()
-        )
-        self.frame = wx.Frame(parent=None, title=__title__)
-        self.menu = Menu(store=self.store, parent=self.frame)
-        if self.store.config.show_setup:
-            self.store.config.show_setup = False
-            self.show_setup()
-        self._tick_start()
-        return True
-
-    def show_menu(self):
-        self.menu.popup_at(*wx.GetMousePosition())
-
-    def show_setup(self):
-        Setup(
-            self.store.config,
-            on_finish=self.on_setup_finish,
-            parent=self.frame
-        )
-
-    def on_setup_finish(self, setup: Setup):
-        self.store.config = setup.config
-        self.menu.refresh()
-        save_config(self.store.config)
-
-    def show_settings(self):
-        Settings(
-            self.store.config,
-            self.on_settings_accept,
-            parent=self.frame
-        )
-
-    def on_settings_accept(self, settings: Settings):
-        self.store.config = settings.config
-        self.menu.refresh()
-        save_config(self.store.config)
-
-    def show_about(self):
-        image = draw_pie_chart(148, list(gen_random_slices(3, 8)))
-        show_about(
-            image=image,
-            title=__title__,
-            version=__version__,
-            copyright=__copyright__,
-            uri=__uri__,
-            authors=__authors__
-        )
-
-    def _tick_start(self):
-        self.tick_event_stop = Event()
-        self.tick_thread = Thread(target=self._tick)
-        self.tick_thread.start()
-
-    def _tick_stop(self):
-        if self.tick_event_stop is not None:
-            self.tick_event_stop.set()
-        if self.tick_thread is not None:
-            self.tick_thread.join()
-        logger.info('Tick stopped')
-
-    def _tick(self):
-        while not self.tick_event_stop.is_set():
-            wx.CallAfter(self._on_tick)
-            sleep(1)
-
-    def _on_tick(self, pulse: bool = True):
-        logger.info('Updating UI')
-        self.status_icon.update()
-        self.menu.update()
+            self.update_menu()
 
     def quit(self):
-        logger.info('Menu quit')
-        self._tick_stop()
+        super().quit()
         self.status_icon.destroy()
-        self.menu.Destroy()
-        self.frame.Destroy()
 
-    def OnExit(self):
-        logger.info('App OnExit')
-        self._tick_stop()
+    def on_exit(self):
         self.on_quit()
-        super().OnExit()
-        return True
 
 
 def run_app(store: Store, on_quit: Callable):
--- lidske_aktivity/ui/lib.py
+++ lidske_aktivity/ui/lib.py
@@ -1,9 +1,14 @@
 import io
-from typing import Callable, List
+import logging
+from threading import Event, Thread
+from time import sleep
+from typing import Callable, List, Optional
 
 import wx
 import wx.adv
 from PIL import Image
+
+logger = logging.getLogger(__name__)
 
 
 def new_id_ref_compat():
@@ -109,14 +114,69 @@ def show_about(image: Image,
 
 
 class Application(wx.App):
-    pass
+    title: str
+    frame: wx.Frame
+
+    tick_event_stop: Optional[Event] = None
+    tick_thread: Optional[Thread] = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(False, *args, **kwargs)
+
+    def OnInit(self) -> bool:
+        self.frame = wx.Frame(parent=None, title=self.title)
+        self.on_init()
+        self._tick_start()
+        return True
+
+    def on_init(self):
+        raise NotImplementedError
+
+    def _tick_start(self):
+        self.tick_event_stop = Event()
+        self.tick_thread = Thread(target=self._tick)
+        self.tick_thread.start()
+
+    def _tick_stop(self):
+        if self.tick_event_stop is not None:
+            self.tick_event_stop.set()
+        if self.tick_thread is not None:
+            self.tick_thread.join()
+        logger.info('Tick stopped')
+
+    def _tick(self):
+        while not self.tick_event_stop.is_set():
+            logger.info('Updating UI')
+            wx.CallAfter(self.on_tick)
+            sleep(1)
+
+    def on_tick(self):
+        raise NotImplementedError
+
+    def quit(self):
+        logger.info('Menu quit')
+        self._tick_stop()
+        self.frame.Destroy()
+
+    def OnExit(self):
+        logger.info('App OnExit')
+        self._tick_stop()
+        self.on_exit()
+        super().OnExit()
+        return True
+
+    def on_exit(self):
+        raise NotImplementedError
+
+    def run(self):
+        self.MainLoop()
 
 
 class StatusIcon(wx.adv.TaskBarIcon):
+    menu: Menu
     id_setup = new_id_ref_compat()
 
     def __init__(self,
-                 on_menu: Callable,
                  on_setup: Callable,
                  on_settings: Callable,
                  on_about: Callable,
@@ -124,11 +184,12 @@ class StatusIcon(wx.adv.TaskBarIcon):
         # TODO: Check TaskBarIcon.isAvailable()
         super().__init__(wx.adv.TBI_DOCK)
         self.update()
-        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, on_menu)
-        self.Bind(wx.EVT_MENU, on_setup, id=self.id_setup)
-        self.Bind(wx.EVT_MENU, on_settings, id=wx.ID_SETUP)
-        self.Bind(wx.EVT_MENU, on_about, id=wx.ID_ABOUT)
-        self.Bind(wx.EVT_MENU, on_quit, id=wx.ID_EXIT)
+        self.menu = Menu(store=self.store, parent=self.frame)
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, self._show_menu)
+        self.Bind(wx.EVT_MENU, lambda event: on_setup(), id=self.id_setup)
+        self.Bind(wx.EVT_MENU, lambda event: on_settings(), id=wx.ID_SETUP)
+        self.Bind(wx.EVT_MENU, lambda event: on_about(), id=wx.ID_ABOUT)
+        self.Bind(wx.EVT_MENU, lambda event: on_quit(), id=wx.ID_EXIT)
 
     def CreatePopupMenu(self) -> wx.Menu:
         menu = wx.Menu()
@@ -138,9 +199,18 @@ class StatusIcon(wx.adv.TaskBarIcon):
         menu.Append(wx.ID_EXIT, 'E&xit')
         return menu
 
+    def _show_menu(self):
+        self.menu.popup_at(*wx.GetMousePosition())
+
     def update_icon(self, image: Image, tooltip: str):
         icon = create_icon_from_image(image)
         self.SetIcon(icon, tooltip)
+
+    def update_menu(self):
+        self.menu.update()
+
+    def refresh_menu(self):
+        self.menu.refresh()
 
     @staticmethod
     def calc_icon_size() -> int:
@@ -150,8 +220,6 @@ class StatusIcon(wx.adv.TaskBarIcon):
             return 22
         return 128
 
-    def run(self):
-        self.MainLoop()
-
     def destroy(self):
         self.Destroy()
+        self.menu.Destroy()
