3f9dfc3ff8d1c1ebafe6af057cca4334776e833f
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-09 21:53:19 +0100

    cli: Fix Wx autodet., add flag --wx instd of --gtk

--- lidske_aktivity/cli.py
+++ lidske_aktivity/cli.py
@@ -2,31 +2,45 @@ import argparse
 import logging
 import sys
 
+from lidske_aktivity import __title__
 from lidske_aktivity.app import Application
 from lidske_aktivity.config import clean_cache
 
 logger = logging.getLogger(__name__)
 
 
-def is_wx_available() -> bool:
+def is_appindicator_available() -> bool:
     try:
-        import wx
-        import wx.adv
-        logger.info('WxWidgets library is available')
-        wx.App()
-        is_tray_available = wx.adv.TaskBarIcon.IsAvailable()
-        logger.info('TaskBarIcon availability = %s', is_tray_available)
-        return is_tray_available
-    except ModuleNotFoundError:
-        logger.info('WxWidgets library is not available')
+        import gi
+        gi.require_version('AppIndicator3', '0.1')
+        logger.info('AppIndicator is available')
+        return True
+    except ValueError:
+        logger.info('AppIndicator is not available')
         return False
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-c', '--clean', action='store_true')
-    parser.add_argument('-g', '--gtk', action='store_true')
+    parser = argparse.ArgumentParser(description=__title__)
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='Enable debugging output'
+    )
+    parser.add_argument(
+        '-c',
+        '--clean',
+        action='store_true',
+        help='Clean cache and exit'
+    )
+    parser.add_argument(
+        '-w',
+        '--wx',
+        action='store_true',
+        help=('Force the use of the WxWidget backend; by default, GTK is '
+              'always used when AppIndicator is available')
+    )
     args = parser.parse_args()
     if args.verbose:
         logging.basicConfig(
@@ -36,8 +50,8 @@ def main():
     if args.clean:
         clean_cache()
     else:
-        if not args.gtk and is_wx_available():
-            import lidske_aktivity.wx as ui
-        else:
+        if not args.wx and is_appindicator_available():
             import lidske_aktivity.gtk as ui
+        else:
+            import lidske_aktivity.wx as ui
         Application(ui)
