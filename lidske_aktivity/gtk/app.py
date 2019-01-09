a73b046647d08c730a42c94289d041fe60d62e39
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-09 15:25:00 +0100

    gtk/app: Use self.window as self.frame

--- lidske_aktivity/gtk/app.py
+++ lidske_aktivity/gtk/app.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Callable
+from typing import Callable, Optional
 
 import gi
 
@@ -13,12 +13,11 @@ logger = logging.getLogger(__name__)
 
 
 class Application(Gtk.Application):
-    frame = None
+    frame: Optional[Gtk.ApplicationWindow] = None
 
     def __init__(self, on_init: Callable, on_quit: Callable, *args, **kwargs):
         self.on_init = on_init
         self.on_quit = on_quit
-        self.window: Gtk.ApplicationWindow = None
         GLib.set_application_name(__title__)
         super().__init__(*args, application_id=__application_id__, **kwargs)
 
@@ -26,6 +25,6 @@ class Application(Gtk.Application):
         Gtk.Application.do_startup(self)
         self.on_init(self.frame)
 
-        if not self.window:
-            self.window = Gtk.ApplicationWindow(application=self)
     def do_activate(self):
+        if not self.frame:
+            self.frame = Gtk.ApplicationWindow(application=self)
