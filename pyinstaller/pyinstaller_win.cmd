1f54789e5edafeb52cf7ff6d43f230b77a7519e9
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-23 05:03:52 -0800

    pyinstaller: Add hidden sqlalchemy import

--- pyinstaller/pyinstaller_win.cmd
+++ pyinstaller/pyinstaller_win.cmd
@@ -4,4 +4,5 @@ pyinstaller ^
     --name=lidske-aktivity ^
     --specpath=pyinstaller ^
     --icon=data\lidske-aktivity.ico ^
+    --hidden-import=sqlalchemy.ext.baked ^
     lidske_aktivity\__main__.py
