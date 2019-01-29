b1cad2b14a1ae0870350cf6ec36fcc9b6f8bc01e
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-29 12:11:58 +0100

    gtk/app: Remove unused CSS

--- lidske_aktivity/gtk/app.py
+++ lidske_aktivity/gtk/app.py
@@ -1,48 +1,17 @@
 import logging
-from typing import Callable, Iterator, Optional, TypeVar
+from typing import Callable, Optional, TypeVar
 
 import gi
 
 from lidske_aktivity import __application_id__, __title__
-from lidske_aktivity.icon import MAX_COLORS, Color, color_from_index
 
-gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gdk, GLib, Gtk  # noqa:E402  # isort:skip
+from gi.repository import GLib, Gtk  # noqa:E402  # isort:skip
 
 logger = logging.getLogger(__name__)
 
 T = TypeVar('T')
-
-
-def gen_bg_color_rule(selector: str, color: Color) -> str:
-    return f'{selector} {{ background: rgb({color.r}, {color.g}, {color.b}) }}'
-
-
-def gen_color_rules() -> Iterator[str]:
-    for i in range(MAX_COLORS):
-        yield gen_bg_color_rule(f'.bg-{i} progress', color_from_index(i))
-    yield gen_bg_color_rule('progress.pulse', color_from_index(-1))
-    yield 'progress { border-color: transparent }'
-
-
-def gen_stylesheet() -> bytes:
-    rules = gen_color_rules()
-    s = ''.join(rules)
-    return s.encode()
-
-
-def load_css():
-    screen = Gdk.Screen.get_default()
-    provider = Gtk.CssProvider()
-    stylesheet = gen_stylesheet()
-    provider.load_from_data(stylesheet)
-    Gtk.StyleContext.add_provider_for_screen(
-        screen,
-        provider,
-        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
-    )
 
 
 class Application(Gtk.Application):
@@ -53,7 +22,6 @@ class Application(Gtk.Application):
         self.on_quit = on_quit
         GLib.set_application_name(__title__)
         super().__init__(*args, application_id=__application_id__, **kwargs)
-        load_css()
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
