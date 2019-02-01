5549e4168e90b09ca2f66248823982c97a00d8ad
Jakub Valenta <jakub@jakubvalenta.cz>
2019-02-01 11:43:43 -0800

    locale: Understand PyInstaller bundle

--- lidske_aktivity/locale.py
+++ lidske_aktivity/locale.py
@@ -1,5 +1,6 @@
 import gettext
 import os
+import os.path
 import sys
 
 from lidske_aktivity import __application_name__
@@ -9,8 +10,11 @@ if sys.platform.startswith('win'):
     if os.getenv('LANG') is None:
         lang, enc = locale.getdefaultlocale()
         os.environ['LANG'] = lang
-    localedir = 'locale'
+
+if getattr(sys, 'frozen', False):  # Running in a bundle
+    localedir = os.path.join(sys._MEIPASS, 'locale')
 else:
     localedir = None
-t = gettext.translation(__application_name__, localedir)
+
+t = gettext.translation(__application_name__, localedir, fallback=True)
 _ = t.gettext
