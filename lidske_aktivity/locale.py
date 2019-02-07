516e87ea43a610c0f58f461f9bcb6c6f20b9ee3d
Jakub Valenta <jakub@jakubvalenta.cz>
2019-02-07 20:36:16 +0100

    locale: Lint

--- lidske_aktivity/locale.py
+++ lidske_aktivity/locale.py
@@ -6,14 +6,13 @@ import sys
 from lidske_aktivity import __application_name__
 from lidske_aktivity.vendor import gettext_windows
 
-translation_kwargs = {}
-if getattr(sys, 'frozen', False):  # Running in a bundle
-    bundle_dir = sys._MEIPASS  # type: ignore
-    translation_kwargs['localedir'] = os.path.join(bundle_dir, 'locale')
-
 gettext_windows.setup_env()
 
-t = gettext.translation(
-    __application_name__, **translation_kwargs, fallback=True
-)
+if getattr(sys, 'frozen', False):  # Running in a bundle
+    bundle_dir = sys._MEIPASS  # type: ignore
+    localedir = os.path.join(bundle_dir, 'locale')
+    t = gettext.translation(__application_name__, localedir, fallback=True)
+else:
+    t = gettext.translation(__application_name__, fallback=True)
+
 _ = t.gettext
